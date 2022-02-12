85cb3ac2dd95eab23c575a77e549dde45b1d98d3
Zhou Haixian <2223540637@qq.com>
2022-02-12 22:56:20 +0800

    Update getDownloadLink.ts

--- api/getDownloadLink.ts
+++ api/getDownloadLink.ts
@@ -3,8 +3,20 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 const https = require('https')
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name } = request.query;
-  response.status(200).send(`
-  <script>console.log(\`Hello ${ name }\`)</script>
-  `);
+  https.get('https://api.github.com/repos/DGP-Studio/Snap.Genshin/releases/latest', result => {
+    let data = ''
+    result.on('data', chunk => {
+      data += chunk;
+    })
+    result.on('end', () => {
+      let downloadLink: string = JSON.parse(data).assets[0]
+      let fastDownloadLink: string = downloadLink.replace('github.com', 'download.fastgit.org')
+      response.status(200).send(`
+        <script>window.location.replace(${fastDownloadLink})</script>
+      `)
+    })
+  }).on('error', error => {
+    response.status(500).send(JSON.stringify(error))
+    console.error(error)
+  })
 };

306adee07a5a1f12f6f0c75d580b195e0277eeab
Zhou Haixian <2223540637@qq.com>
2022-02-12 21:32:06 +0800

    测试api返回的script是否会被执行

--- api/getDownloadLink.ts
+++ api/getDownloadLink.ts
@@ -1,7 +1,10 @@
 // @ts-ignore
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+const https = require('https')
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  response.status(200).send(`
+  <script>console.log(\`Hello ${ name }\`)</script>
+  `);
 };

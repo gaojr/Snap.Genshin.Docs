64ee124e36ea098552f8a7ab624260b9cca2332f
Zhou Haixian <2223540637@qq.com>
2022-02-13 09:30:48 +0800

    Update getDownloadLink.ts

--- api/getDownloadLink.ts
+++ api/getDownloadLink.ts
@@ -3,14 +3,18 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 const https = require('https')
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  https.get('https://api.github.com/repos/DGP-Studio/Snap.Genshin/releases/latest', result => {
+  https.get({
+    host: 'api.github.com',
+    path: '/repos/DGP-Studio/Snap.Genshin/releases/latest',
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
+    }
+  }, result => {
     let data;
     result.on('data', chunk => {
       data += chunk;
     })
     result.on('end', () => {
-      console.warn(data)
-      console.warn(typeof data)
       let downloadLink: string = JSON.parse(data).assets[0].browser_download_url
       let fastDownloadLink: string = downloadLink.replace('github.com', 'download.fastgit.org')
       response.status(200).send(`

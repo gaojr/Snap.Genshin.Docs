7a555126328fdb03cf1066ac2eff5b24aa9f844a
Zhou Haixian <2223540637@qq.com>
2022-03-12 18:41:47 +0800

    [Fix bug] 与广告过滤器猫捉老鼠

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,7 @@ export default defineUserConfig<DefaultThemeOptions>({
     ["script", { src: "https://cdn.jsdelivr.net/npm/cfga@1.0.3", async: true }],
     ["script", {}, `
       window.ga_tid = "UA-222060450-1";
-      window.ga_api = "https://da.snapgenshin.cn/cfga/jquery.js";
+      window.ga_api = "https://da.snapgenshin.cn";
     `]
   ],
   lang: 'zh-CN',
